f8f14cfece898e607da7027d94da07bcc194dfd3
firman196 <firmanagussaputro970@gmail.com>
2023-06-10 13:21:03 +0700

    add feature auto migrate database

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	database "Jakpat_Test_2/database/mysql"
+	"Jakpat_Test_2/models"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+func main() {
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatal().Err(envErr).Msg("cannot load environment")
+	}
+
+	appPort := os.Getenv("PORT")
+	if appPort == "" {
+		appPort = "8080"
+	}
+
+	var dbUsername = os.Getenv("DB_USERNAME")
+	var dbPassword = os.Getenv("DB_PASSWORD")
+	var dbName = os.Getenv("DB_DATABASE")
+	var dbHost = os.Getenv("DB_HOST")
+	var dbPort = os.Getenv("DB_PORT")
+	db, dbErr := database.ConnectDB(dbUsername, dbPassword, dbHost, dbPort, dbName)
+
+	if dbErr != nil {
+		log.Fatal().Err(dbErr).Msg("cannot connect to database")
+	}
+
+	//auto migrate database
+	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.Inventory{})
+	db.AutoMigrate(&models.SalesOrder{})
+
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	// route swagger
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	//api := router.Group("/api/v1")
+
+	router.Run(":" + appPort)
+
+}
