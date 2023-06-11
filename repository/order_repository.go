3e0b4ff69a55c7799255c8498d53eb092113af81
firman196 <firmanagussaputro970@gmail.com>
2023-06-11 17:09:34 +0700

    add repository order

--- repository/order_repository.go
+++ repository/order_repository.go
@@ -1,1 +1,10 @@
 package repository
+
+import "Jakpat_Test_2/models"
+
+type OrderRepository interface {
+	Create(order models.SalesOrder) (*models.SalesOrder, error)
+	Update(order models.SalesOrder) (*models.SalesOrder, error)
+	FindByID(id string) (*models.SalesOrder, error)
+	FindBySellerId(sellerId int) ([]models.SalesOrder, error)
+}
--- repository/order_repository_impl.go
+++ repository/order_repository_impl.go
@@ -1,1 +1,55 @@
 package repository
+
+import (
+	"Jakpat_Test_2/models"
+
+	"gorm.io/gorm"
+)
+
+type OrderRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewOrderRepositoryImpl(db *gorm.DB) OrderRepository {
+	return &OrderRepositoryImpl{
+		db: db,
+	}
+}
+
+func (r *OrderRepositoryImpl) Create(order models.SalesOrder) (*models.SalesOrder, error) {
+	err := r.db.Create(&order).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &order, nil
+}
+
+func (r *OrderRepositoryImpl) Update(order models.SalesOrder) (*models.SalesOrder, error) {
+	err := r.db.Model(&order).Where("order_id = ?", order.OrderId).Where("is_deleted =?", false).Updates(&order).Error
+	if err != nil {
+		return nil, err
+	}
+	return &order, nil
+}
+
+func (r *OrderRepositoryImpl) FindByID(id string) (*models.SalesOrder, error) {
+	var order models.SalesOrder
+	err := r.db.Model(&order).Where("order_id =?", id).First(&order).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &order, nil
+}
+
+func (r *OrderRepositoryImpl) FindBySellerId(sellerId int) ([]models.SalesOrder, error) {
+	var order []models.SalesOrder
+
+	err := r.db.Where("seller_id =?", sellerId).Preload("User").Find(&order).Error
+	if err != nil {
+		return order, err
+	}
+
+	return order, nil
+}
